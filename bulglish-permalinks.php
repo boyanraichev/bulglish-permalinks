f50a12723c6a12e4d0d39b7f42b141a15c6e58bb
studioreforma <studioreforma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-24 13:14:01 +0000

    Added ver 1.0
    git-svn-id: http://plugins.svn.wordpress.org/bulglish-permalinks/trunk@901684 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bulglish-permalinks.php
@@ -0,0 +1,54 @@
+<?php 
+/*
+Plugin Name: Bulglish permalinks
+Plugin URI: http://talkingaboutthis.eu
+Description: This plugins provides bulglish permalinks for cyrillic titles. Based on the work of Iacovos Constantinou
+Author: Boyan Raichev 
+Version: 1.0
+Author URI: http://talkingaboutthis.eu/
+*/
+
+// Based on http://www.freestuff.gr/forums/viewtopic.php?p=194579#194579
+function bulglish_permalinks_title($text) {
+	if ( !is_admin() ) return $text;
+	
+	$expressions = array(
+		'/[иИ][яЯ]/u' => 'ia',
+		'/[аА]/u' => 'a',
+		'/[бБ]/u' => 'b',
+		'/[вВ]/u' => 'v',
+		'/[гГ]/u' => 'g',
+		'/[дД]/u' => 'd',
+		'/[ѐеЕ]/u' => 'e',
+		'/[жЖ]/u' => 'zh',
+		'/[зЗ]/u' => 'z',
+		'/[иИ]/u' => 'i',
+		'/[ѝЍ]/u' => 'i',
+		'/[йЙ]/u' => 'y',
+		'/[кК]/u' => 'k',
+		'/[лЛ]/u' => 'l',
+		'/[мМ]/u' => 'm',
+		'/[нН]/u' => 'n',
+		'/[оО]/u' => 'o',
+		'/[пП]/u' => 'p',
+		'/[рР]/u' => 'r',
+		'/[сС]/u' => 's',
+		'/[тТ]/u' => 't',
+		'/[уУ]/u' => 'u',
+		'/[фФ]/u' => 'f',
+		'/[хХ]/u' => 'h',
+		'/[цЦ]/u' => 'ts',
+		'/[чЧ]/u' => 'ch',
+		'/[шШ]/u' => 'sh',
+		'/[щЩ]/u' => 'sht',
+		'/[Ъъ]/u' => 'a',
+		'/[ь]/u' => 'y',
+		'/[юЮ]/u' => 'yu',
+		'/[яЯ]/u' => 'ya'		
+	);
+	
+	$text = preg_replace( array_keys($expressions), array_values($expressions), $text );
+	return $text;
+}
+add_filter('sanitize_title', 'bulglish_permalinks_title', 1);
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Bulglish Permalinks ===
+Contributors: studioreforma
+Donate link: http://talkingaboutthis.eu/
+Tags: bulgarian, bulgarian permalink, cyrillic, slugs, transliteration 
+Requires at least: 3.0.1
+Tested up to: 3.9.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin converts Bulgarian cyrillic characters in slugs to Latin characters, according to the official rules for transliteration. 
+
+== Description ==
+
+This plugin converts Bulgarian cyrillic characters in slugs to Latin characters, according to the official rules for transliteration.
+
+Cyrillic letters in URLs, while supported, are ugly and when copy/pasted around the internet are often encoded (converted to those ugly URLs looking like %DU^ED%…)
+The Plugin is based on the
+
+= Features =
+
+* Automatically converts existing post, page and taxonomy slugs when you save them
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0 =
+* First release
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+First release
+
